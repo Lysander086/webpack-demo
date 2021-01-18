a805ff0b97154c25e0e9667ed57d56f73b36e9a5
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-01-18 11:37:27 +0800

    处理图片 fail, 浏览器显示图片失败

--- app/index.js
+++ app/index.js
@@ -2,5 +2,7 @@
 // var sum = require('./sum')
 
 import sum from './sum'
+import {appendImages} from "./addImage"
 
 console.log(sum(1, 2))
+appendImages()
--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,31 @@ module.exports = {
   // 2 output
   output: {
     path: path.resolve(__dirname, 'build'), // 必须使用绝对地址，输出文件夹
-    filename: "bundle.js" // 打包后输出文件的文件名
+    filename: "bundle.js",// 打包后输出文件的文件名
+    publicPath: 'build/',// 知道如何寻找资源
   },
 
   // 3 loaders
   module: {
     rules: [
-      //  js use babel
-      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
-      
+      //  js babel-loader
+      {test: /\.js$/, use: 'babel-loader', exclude: /node_modules/},
+      // deal with images
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [{
+          loader: 'url-loader',
+          // 配置 url-loader 的可选项
+          options: {
+            // 限制 图片大小 10000B，小于限制会将图片转换为 base64格式
+            limit: 10000,
+            // 超出限制，创建的文件格式
+            // build/images/[图片名].[hash].[图片格式]
+            name: 'images/[name].[hash].[ext]'
+          }
+        }]
+      },
+
     ]
   }
 }
